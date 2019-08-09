ad97305242e86bb8ec4f29f25e99bd9dd0ccc4f6
root-yang <1048710667@qq.com>
2019-08-09 13:55:01 +0800

    Site updated: 2019-08-09 13:54:54

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/root-jing/root-jing.github.io/master/asset/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/root-jing/root-jing.github.io/master/asset/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/root-jing/blogMedias/master/imgs/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/root-jing/blogMedias/master/imgs/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
